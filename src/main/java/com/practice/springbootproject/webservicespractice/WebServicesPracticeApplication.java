f8d18382890c882875d5d20b8f3d308186548aa8
Himanshu Garg <hgarg701@gmail.com>
2020-12-29 16:55:35 -0800

    Initalized web-services project

---
+++ src/main/java/com/practice/springbootproject/webservicespractice/WebServicesPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springbootproject.webservicespractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/springbootproject/webservicespractice/WebServicesPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springbootproject.webservicespractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

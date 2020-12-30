4c98d0081e905192abe2d36653f7829f1725f3f4
Himanshu Garg <hgarg701@gmail.com>
2020-12-29 18:47:13 -0800

    Handling request with path variable

--- src/main/java/com/practice/springbootproject/webservicespractice/HelloController.java
+++ src/main/java/com/practice/springbootproject/webservicespractice/HelloController.java
@@ -1,6 +1,7 @@
 package com.practice.springbootproject.webservicespractice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloController {
     @GetMapping(path="hello-bean")
     public HelloBean sayHelloBean(){
         return new HelloBean("Hello Bean");
+    }
+
+    @GetMapping(path="hello-bean/{name}")
+    public HelloBean sayHelloWithPath(@PathVariable String name){
+        return new HelloBean("Hello There " + name);
     }
 }
